c9e4f165af28302cfd5c0ec9a3ffe980cc5185b4
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-08-03 00:22:46 +0300

    add RepairTask class with creation and update methods, including validation errors

---
+++ src/MechanicShop.Domain/RepairTasks/RepairTask.cs
@@ -0,0 +1,67 @@
+using MechanicShop.Domain.Common;
+using MechanicShop.Domain.Common.Results;
+using MechanicShop.Domain.RepairTasks.Enums;
+using MechanicShop.Domain.RepairTasks.Parts;
+
+namespace MechanicShop.Domain.RepairTasks;
+
+public sealed class RepairTask:AuditableEntity
+{
+    public string? Name {get;private set;}
+    public decimal LaborCost {get; private set;}
+    public RepairDurationInMinutes EstimatedDurationInMinutes {get;private set;}
+    private readonly List<Part>? _parts = [];
+    public IEnumerable<Part> Parts => _parts!.AsReadOnly();
+    public  decimal TotalCost => Parts!.Sum(p=>p.Cost * p.Quantity);
+
+    private RepairTask(Guid id,string? name, decimal laborCost, RepairDurationInMinutes estimatedDurationInMinutes, List<Part>? parts):base(id)
+    {
+        Name = name;
+        LaborCost = laborCost;
+        EstimatedDurationInMinutes = estimatedDurationInMinutes;
+        _parts = parts;
+    }
+    public static Result<RepairTask> Create(Guid id, string name, decimal laborCost, RepairDurationInMinutes estimatedDurationInMins, List<Part> parts)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return RepairTaskErrors.NameRequired;
+        }
+
+        if (laborCost <= 0)
+        {
+            return RepairTaskErrors.LaborCostInvalid;
+        }
+
+        if (!Enum.IsDefined(estimatedDurationInMins))
+        {
+            return RepairTaskErrors.DurationInvalid;
+        }
+
+        return new RepairTask(id, name.Trim(), laborCost, estimatedDurationInMins, parts);
+    }
+        public Result<Updated> Update(string name, decimal laborCost, RepairDurationInMinutes estimatedDurationInMins)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return RepairTaskErrors.NameRequired;
+        }
+
+        if (laborCost <= 0 || laborCost > 10000)
+        {
+            return RepairTaskErrors.LaborCostInvalid;
+        }
+
+        if (!Enum.IsDefined(estimatedDurationInMins))
+        {
+            return RepairTaskErrors.DurationInvalid;
+        }
+
+        Name = name.Trim();
+        LaborCost = laborCost;
+        EstimatedDurationInMinutes = estimatedDurationInMins;
+
+        return Result.Updated;
+    }
+
+}
