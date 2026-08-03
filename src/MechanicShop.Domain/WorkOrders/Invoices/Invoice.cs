7795a02825249b61bc3ada82101b40f1c92dc163
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-08-03 15:36:22 +0300

    refactor Invoice class for improved error handling and validation; add InvoiceErrors class for centralized error management

--- src/MechanicShop.Domain/WorkOrders/Invoices/Invoice.cs
+++ src/MechanicShop.Domain/WorkOrders/Invoices/Invoice.cs
@@ -6,8 +6,9 @@ namespace MechanicShop.Domain.WorkOrders.Invoices;
 
 public sealed class Invoice : AuditableEntity
 {
-    public Guid WorkOrderId {get;private set;}
-    public DateTimeOffset IssuedAtUtc {get;private set;}
+    public Guid WorkOrderId {get;}
+    public WorkOrder WorkOrder {get;set;}
+    public DateTimeOffset IssuedAtUtc {get;}
     public decimal DiscountAmount {get;private set;}
     public decimal TaxAmount {get;private set;}
     public InvoiceStatus Status {get;private set;}
@@ -22,32 +23,28 @@ public sealed class Invoice : AuditableEntity
         
     }
 
-    private Invoice(Guid id,Guid workOrderId, decimal discountAmount,decimal taxAmount,List<InvoiceLineItem> lineItems):base(id)
+    private Invoice(Guid id,Guid workOrderId, decimal discountAmount,decimal taxAmount,DateTimeOffset issuedAt,List<InvoiceLineItem> lineItems):base(id)
     {
         WorkOrderId = workOrderId;
-        IssuedAtUtc = DateTimeOffset.UtcNow;
+        IssuedAtUtc = issuedAt;
         DiscountAmount = discountAmount;
         TaxAmount= taxAmount;
         _invoiceLineItems= lineItems; 
         Status = InvoiceStatus.UnPaid;
     }
 
-    public static Result<Invoice> Create(Guid id,Guid workOrderId, decimal discountAmount,decimal taxAmount,List<InvoiceLineItem> lineItems)
+    public static Result<Invoice> Create(Guid id,Guid workOrderId, decimal discountAmount,decimal taxAmount,TimeProvider timeProvider,List<InvoiceLineItem> lineItems)
     {
         if (workOrderId == Guid.Empty)
         {
             return InvoiceErrors.WorkOrderIdInvalid;
         }
-        if (discountAmount <= 0)
-        {
-            return InvoiceErrors.DiscountNegative;
-        }
-        if (lineItems.Count <=0 || lineItems is null)
+        if (lineItems.Count ==0 || lineItems is null)
         {
             return InvoiceErrors.LineItemsEmpty;
         }
 
-        return new Invoice(id,workOrderId,discountAmount,taxAmount,lineItems);
+        return new Invoice(id,workOrderId,discountAmount,taxAmount,timeProvider.GetUtcNow(),lineItems);
     }
 
     public Result<Updated> ApplyDiscount(decimal amount)
@@ -56,49 +53,25 @@ public sealed class Invoice : AuditableEntity
         {
             return InvoiceErrors.InvoiceLocked;
         }
-        if (amount >= SubTotal || amount <= 0)
+        if (amount > SubTotal)
         {
             return  InvoiceErrors.DiscountExceedsSubtotal;
         }
-        if ( amount <= 0)
+        if ( amount < 0)
         {
             return  InvoiceErrors.DiscountNegative;
         }
         DiscountAmount = amount;
-
         return Result.Updated;
     }
-    public Result<Updated> MarkAsPaid()
+    public Result<Updated> MarkAsPaid(TimeProvider timeProvider)
     {
         if (Status!= InvoiceStatus.UnPaid)
         {
             return InvoiceErrors.InvoiceLocked;
         }
-        PaidAt = DateTimeOffset.UtcNow;
-        
+        Status = InvoiceStatus.Paid;
+        PaidAt =timeProvider.GetUtcNow();
         return Result.Updated;
     }
-}
-
-public static class InvoiceErrors
-{
-    public static readonly Error WorkOrderIdInvalid = Error.Validation(
-        code: "Invoice.WorkOrderId.Invalid",
-        description: "WorkOrderId is invalid");
-
-    public static readonly Error LineItemsEmpty = Error.Validation(
-        code: "Invoice.LineItems.Empty",
-        description: "Invoice must have line items");
-
-    public static readonly Error InvoiceLocked = Error.Validation(
-        code: "Invoice.Locked",
-        description: "Invoice is locked");
-
-    public static readonly Error DiscountNegative = Error.Validation(
-        code: "Invoice.Discount.Negative",
-        description: "Discount cannot be negative");
-
-    public static readonly Error DiscountExceedsSubtotal = Error.Validation(
-        code: "Invoice.Discount.ExceedsSubtotal",
-        description: "Discount exceeds subtotal");
 }
