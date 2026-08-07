a32971d17e125f1d3d90c4524de4687052c00293
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-08-07 23:56:09 +0300

    refactor: reorganize customer query handling and update caching key

--- src/MechanicShop.Application/Features/RepairTasks/Queries/GetRepairTasks/GetRepairTasksQuery.cs
+++ src/MechanicShop.Application/Features/RepairTasks/Queries/GetRepairTasks/GetRepairTasksQuery.cs
@@ -6,7 +6,7 @@ namespace MechanicShop.Application.Features.RepairTasks.Queries.GetRepairTasks;
 
 public sealed record GetRepairTasksQuery() : ICachedQuery<Result<List<RepairTaskDto>>>
 {
-    public string CacheKey => "repair-tasks";
+    public string CachingKey => "repair-tasks";
 
     public TimeSpan Expiration => TimeSpan.FromMinutes(10);
 
