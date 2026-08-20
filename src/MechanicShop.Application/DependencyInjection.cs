ca7983ecf1940e62db155a4836d245c0e08a8621
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-08-20 16:24:28 +0300

    feat: add DependencyInjection class for application services registration

---
+++ src/MechanicShop.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using FluentValidation;
+using MechanicShop.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MechanicShop.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(PerformanceBehavior<,>));
+            cfg.AddOpenBehavior(typeof(UnhandledExceptionBehavior<,>));
+            cfg.AddOpenBehavior(typeof(CachingBehavior<,>));
+        });
+
+        return services;
+    }
+}
