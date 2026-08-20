948825b67b71ffa0aebae16a2165410c43863ec6
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-08-20 18:18:14 +0300

    refactor: update BearerSecuritySchemeTransformer and VersionInfoTransformer for consistency in OpenApi usage

--- src/MechanicShop.Api/OpenApi/Transformers/BearerSecuritySchemeTransformer.cs
+++ src/MechanicShop.Api/OpenApi/Transformers/BearerSecuritySchemeTransformer.cs
@@ -1,59 +1,61 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.OpenApi;
-using Microsoft.OpenApi.Models;
-
+using Microsoft.OpenApi;
 namespace MechanicShop.Api.OpenApi.Transformers;
 
-internal sealed class BearerSecuritySchemeTransformer : IOpenApiDocumentTransformer, IOpenApiOperationTransformer
+internal sealed class BearerSecuritySchemeTransformer :
+    IOpenApiDocumentTransformer,
+    IOpenApiOperationTransformer
 {
     private const string SchemeId = JwtBearerDefaults.AuthenticationScheme;
 
-    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    public Task TransformAsync(
+        OpenApiDocument document,
+        OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken)
     {
         document.Components ??= new();
-        document.Components.SecuritySchemes ??= new Dictionary<string, OpenApiSecurityScheme>();
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
 
         document.Components.SecuritySchemes[SchemeId] = new OpenApiSecurityScheme
         {
             Type = SecuritySchemeType.Http,
             Scheme = "bearer",
             BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "Enter JWT Bearer token",
             Name = "Authorization",
-            Reference = new OpenApiReference
-            {
-                Type = ReferenceType.SecurityScheme,
-                Id = SchemeId
-            }
+            In = ParameterLocation.Header,
+            Description = "Enter JWT Bearer token."
         };
 
         return Task.CompletedTask;
     }
 
-    public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+    public Task TransformAsync(
+        OpenApiOperation operation,
+        OpenApiOperationTransformerContext context,
+        CancellationToken cancellationToken)
     {
-        if (context.Description.ActionDescriptor.EndpointMetadata.OfType<IAuthorizeData>().Any())
-        {
-            operation.Security ??= [];
+        // System.Console.WriteLine("HelloWorld");
+        var metadata = context.Description.ActionDescriptor.EndpointMetadata;
 
-            var key = new OpenApiSecurityScheme
+        if (metadata.OfType<AllowAnonymousAttribute>().Any())
+            return Task.CompletedTask;
+
+        if (!metadata.OfType<AuthorizeAttribute>().Any())
+            return Task.CompletedTask;
+
+        operation.Security ??= [];
+
+        operation.Security.Add(
+            new OpenApiSecurityRequirement
             {
-                Reference = new OpenApiReference()
-            };
-
-            key.Reference.Type = ReferenceType.SecurityScheme;
-            key.Reference.Id = SchemeId;
-
-            var requirement = new OpenApiSecurityRequirement
-            {
-                { key, [] },
-            };
-
-            operation.Security.Add(requirement);
-        }
-
+                [
+                    new OpenApiSecuritySchemeReference(
+                        SchemeId,
+                        context.Document)
+                ] = []
+            });
         return Task.CompletedTask;
     }
 }
